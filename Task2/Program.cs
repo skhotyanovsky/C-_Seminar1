93c0bd702a73c197618379d9921ff364eaa71e24
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-02-23 13:06:56 +0200

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите номер дня недели:");
+int day = int. Parse(Console.ReadLine());
+switch (day)
+{
+    case 1:
+    Console.WriteLine("Monday");
+    break;
+    case 2:
+    Console.WriteLine("Tuesday");
+    
+}
+
+int numberA = 1;
+int numberB = 2;
+int numberC = 3;
+
+
+if(1)
+{
+Console.WriteLine("Monday");
+}
