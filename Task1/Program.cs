ab4a5376628fabdb117fcc1ef74a9c2727060d6f
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-02-23 13:11:34 +0200

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB * numberB)
+{
+Console.WriteLine("This is true");
+}
+else
+{
+Console.WriteLine("Wrong");
+}
+
